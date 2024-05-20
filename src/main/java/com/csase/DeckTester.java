90530a630767fb2d3931e7f3345cb7ea96a5eff5
Yezen Alboslani <yezenalboslani150322@gmail.com>
2024-05-19 23:55:28 -0400

    Activities 1 & 2

---
+++ src/main/java/com/csase/DeckTester.java
@@ -0,0 +1,30 @@
+package com.csase;
+/**
+ * This is a class that tests the Deck class.
+ */
+public class DeckTester {
+
+	/**
+	 * The main method in this class checks the Deck operations for consistency.
+	 *	@param args is not used.
+	 */
+	public static void main(String[] args) {
+		String[] ranks = {"jack", "queen", "king"};
+		String[] suits = {"blue", "red", "yellow"};
+		int[] pointValues = {11, 12, 13};
+		Deck d = new Deck(ranks, suits, pointValues);
+
+		System.out.println(d.size());
+		System.out.println(d.isEmpty());
+		System.out.println(d.deal().toString());
+		System.out.println(d.size());
+		System.out.println(d.isEmpty());
+		System.out.println(d.deal().toString());
+		System.out.println(d.size());
+		System.out.println(d.isEmpty());
+		System.out.println(d.deal().toString());
+		System.out.println(d.size());
+		System.out.println(d.isEmpty());
+		System.out.println(d.deal().toString());
+	}
+}
